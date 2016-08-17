d05642f0f9c98ec9cc26a76f34aac089e2755efc
jon <j.klang@chronometriq.ca>
2016-08-16 22:25:11 -0400

    continuing auth0 integration with auth filters

--- src/main/java/ca/ibu/api/api/filter/AuthenticationFilter.java
+++ src/main/java/ca/ibu/api/api/filter/AuthenticationFilter.java
@@ -1,34 +1,44 @@
 /**
  * @author chr0n0metriq: jk
  * 
- * ca.ibu.api.api.filter
- * AuthenticationFilter.java
- * Aug 15, 2016
+ *         ca.ibu.api.api.filter
+ *         AuthenticationFilter.java
+ *         Aug 15, 2016
  * 
  */
 package ca.ibu.api.api.filter;
 
 // java
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+// import java.util.Base64;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 // javax
 import javax.annotation.Priority;
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.NotAuthorizedException;
 import javax.ws.rs.Priorities;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 // auth0
 import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.JWTVerifyException;
 
 // ibu
 import ca.ibu.api.api.annotation.Secured;
-
 
 /**
  * @author jk
@@ -41,17 +51,92 @@ public class AuthenticationFilter implements ContainerRequestFilter {
 
     private JWTVerifier jwtVerifier;
     static final Logger LOG = LoggerFactory.getLogger(AuthorizationFilter.class);
-    
-    @Context
-    HttpServletRequest request;
 
-    /* (non-Javadoc)
+    @Context
+    HttpServletRequest  request;
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see javax.ws.rs.container.ContainerRequestFilter#filter(javax.ws.rs.container.ContainerRequestContext)
      */
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
-        // TODO Auto-generated method stub
 
+        String token = null;
+        String username = null;
+        Map<String, Object> decoded = null;
+
+        new Base64(true);
+        // instanciate token verifier object
+        // Base64.getDecoder().decode(encoded);
+        // jwtVerifier = new JWTVerifier(Base64.getDecoder().decode(System.getenv("AUTH0_CLIENT_SECRET")), System.getenv("AUTH0_CLIENT_ID"));
+        jwtVerifier = new JWTVerifier(Base64.decodeBase64(System.getenv("AUTH0_CLIENT_SECRET")), System.getenv("AUTH0_CLIENT_ID"));
+
+        try {
+            // get token from header
+            token = getToken(requestContext);
+
+            // Validate the token
+            decoded = validateToken(token);
+        }
+        catch (Exception e) {
+            LOG.error("Exception while validating token.", e);
+            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
+        }
+    }
+
+    /**
+     * @param requestContext
+     * @return
+     * @throws NotAuthorizedException
+     */
+    private String getToken(ContainerRequestContext requestContext) throws NotAuthorizedException {
+        // logger.traceEntry();
+        String token = null;
+
+        // Get the HTTP Authorization header from the request
+        String authorizationHeader = requestContext.getHeaderString(HttpHeaders.AUTHORIZATION);
+
+        // Check if the HTTP Authorization header is present and formatted correctly
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            LOG.warn("Authorization header must be provided");
+            throw new NotAuthorizedException("Authorization header must be provided");
+        }
+
+        // Extract the token from the HTTP Authorization header
+        String[] parts = authorizationHeader.split(" ");
+
+        if (parts.length != 2) {
+            LOG.warn("Unauthorized: Format is Authorization: Bearer [token]");
+            throw new NotAuthorizedException("Unauthorized: Format is Authorization: Bearer [token]");
+        }
+
+        String scheme = parts[0];
+        String credentials = parts[1];
+
+        Pattern pattern = Pattern.compile("^Bearer$", Pattern.CASE_INSENSITIVE);
+        if (pattern.matcher(scheme).matches()) {
+            token = credentials;
+        }
+
+        return token;
+    }
+    
+    /**
+     * @param token
+     * @return
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidKeyException
+     * @throws IllegalStateException
+     * @throws IOException
+     * @throws SignatureException
+     * @throws JWTVerifyException
+     */
+    private Map<String, Object> validateToken(String token) throws NoSuchAlgorithmException, InvalidKeyException, IllegalStateException, IOException, SignatureException, JWTVerifyException {
+        //logger.traceEntry();
+
+        return logger.traceExit(jwtVerifier.verify(token));
     }
 
 }
